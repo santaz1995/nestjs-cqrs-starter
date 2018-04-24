f7627cbfb763adcdb02d71787c112c80de28d1cb
santaz1995 <janjo@live.ru>
2018-04-24 21:37:58 +0300

    Hotfix add project skill to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { FeedbackModule } from './http/modules/feedbacks/feedback.module';
 import { ProjectCategoryModule } from './http/modules/project-categories/project-category.module';
 import { ProjectModule } from './http/modules/project/project.module';
 import { UploadModule } from './http/modules/upload/upload.module';
+import { ProjectSkill } from './domains/project-skill/project-skill';
 
 @Module({
     modules: [
         FeedbackModule,
-        ProjectCategoryModule,
         ProjectModule,
+        ProjectCategoryModule,
+        ProjectSkill,
         UploadModule
     ],
     components: [
